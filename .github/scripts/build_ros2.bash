2663ba17d0225525d68b55fc8a796eeba0f22f0e
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2022-05-25 09:53:53 -0400

    more debugging...

--- .github/scripts/build_ros2.bash
+++ .github/scripts/build_ros2.bash
@@ -17,9 +17,9 @@ echo "found ros version: ${ROS_VERSION} distro: ${ROS_DISTRO}"
 # run wstool to bring in the additional repositories required
 wstool init src ./src/flir_spinnaker_ros2/flir_spinnaker_ros2.rosinstall
 
-apt list --installed | grep -i 'ament-clang-format'
-ls -la /opt/ros/${ROS_DISTRO}/share/ament_cmake_clang_format/cmake/
-find /opt/ros/${ROS_DISTRO}
+dpkg -L ros-${ROS_DISTRO}-ament-cmake-clang-format
+ls -la /opt/ros/${ROS_DISTRO}/share/ament_cmake*
+find /opt/ros/${ROS_DISTRO}/share/ament_cmake*
 
 # build and test
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo && colcon test && colcon test-result --verbose
