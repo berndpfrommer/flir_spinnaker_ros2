8ed3497e5570ca078ecb9200834c6986ee6fd3d0
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2022-08-26 10:08:17 -0400

    sanitized to pass tests

--- launch/blackfly_s_gige.launch.py
+++ launch/blackfly_s_gige.launch.py
@@ -1,3 +1,19 @@
+# -----------------------------------------------------------------------------
+# Copyright 2022 Kevin Janesch
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration as LaunchConfig
 from launch.actions import DeclareLaunchArgument as LaunchArg
@@ -8,7 +24,7 @@ camera_params = {
     'debug': False,
     'compute_brightness': False,
     'dump_node_map': False,
-    # set parameters defined in blackfly_s_gige.cfg    
+    # set parameters defined in blackfly_s_gige.cfg
     'gain_auto': 'Continuous',
     'exposure_auto': 'Continuous',
     'frame_rate_auto': 'Off',
@@ -27,23 +43,23 @@ camera_params = {
     'gev_scps_packet_size': 9000,
     }
 
+
 def generate_launch_description():
-    """launch blackfly_s camera node."""
+    """Launch blackfly_s camera node."""
     flir_dir = get_package_share_directory('flir_spinnaker_ros2')
     config_dir = flir_dir + '/config/'
     name_arg = LaunchArg('camera_name', default_value='blackfly_s',
                          description='camera name')
     serial_arg = LaunchArg('serial', default_value="'20435008'",
-                         description='serial number')
-    print([LaunchConfig('serial'),'_'])
+                           description='serial number')
+    print([LaunchConfig('serial'), '_'])
     node = Node(package='flir_spinnaker_ros2',
                 executable='camera_driver_node',
                 output='screen',
                 name=[LaunchConfig('camera_name')],
-                parameters=[camera_params,
-                        {'parameter_file': config_dir + 'blackfly_s_gige.cfg',
-                         'serial_number': [LaunchConfig('serial')],
-                        }],
-                remappings=[('~/control', '/exposure_control/control'),],
-    )
+                parameters=[
+                    camera_params,
+                    {'parameter_file': config_dir + 'blackfly_s_gige.cfg',
+                     'serial_number': [LaunchConfig('serial')], }],
+                remappings=[('~/control', '/exposure_control/control')])
     return LaunchDescription([name_arg, serial_arg, node])
